27cd760869a89b0f34e873f4f058ad45e2108fc1
Philippe Vinchon <p.vinchon@gmail.com>
2025-01-21 22:21:04 +0000

    Cache templates to avoid unecessary remote requests

--- internal/template/read.go
+++ internal/template/read.go
@@ -1,19 +1,79 @@
 package template
 
 import (
+	"encoding/base64"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
 func Read(path string) string {
-	if strings.HasPrefix(path, "http") || strings.HasPrefix(path, "https") {
-		return readRemote(path)
+	content := readCache(path)
+	if content != "" {
+		return content
 	}
 
-	return readLocal(path)
+	if isRemote(path) {
+		content = readRemote(path)
+	} else {
+		content = readLocal(path)
+	}
+
+	writeCache(path, content)
+
+	return content
+}
+
+func cachePath(path string) string {
+	userCacheDir, err := os.UserCacheDir()
+	if err != nil {
+		log.Printf("error getting user cache directory: %v", err)
+		return ""
+	}
+
+	encodedPath := base64.RawURLEncoding.EncodeToString([]byte(path))
+
+	return filepath.Join(userCacheDir, "tdd", "templates", encodedPath)
+}
+
+func readCache(path string) string {
+	cachePath := cachePath(path)
+	if cachePath == "" {
+		return ""
+	}
+
+	content, err := os.ReadFile(cachePath)
+	if err != nil {
+		return ""
+	}
+
+	return string(content)
+}
+
+func writeCache(path, content string) {
+	cachePath := cachePath(path)
+	if cachePath == "" {
+		return
+	}
+
+	cacheDir := filepath.Dir(cachePath)
+
+	err := os.MkdirAll(cacheDir, os.ModePerm)
+	if err != nil {
+		log.Fatalf("error creating cache directory %s: %v", cacheDir, err)
+	}
+
+	err = os.WriteFile(cachePath, []byte(content), 0644)
+	if err != nil {
+		log.Fatalf("error writing to cache file %s: %v", cachePath, err)
+	}
+}
+
+func isRemote(path string) bool {
+	return strings.HasPrefix(path, "http") || strings.HasPrefix(path, "https")
 }
 
 func readRemote(url string) string {
